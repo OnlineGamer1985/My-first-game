6985010e35f03accf1c276e4bd43746059ce92fb
OnlineGamer1985 <115266310+OnlineGamer1985@users.noreply.github.com>
2023-01-21 16:47:53 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var ball
+function setup() {
+  createCanvas(800,800);
+  ball=createSprite(400,400,20,20)
+  ball.shapeColor="blue"
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+}
+
+
+
+
